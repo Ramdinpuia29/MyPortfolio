a43dbde31c8fe65ac568ff324a5e25e508c66969
Ramdinpuia29 <Rdpahmar@gmail.com>
2021-05-17 22:13:23 +0530

    Commited

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -44,7 +44,6 @@ class Header extends Component {
                 Resume
               </a>
             </li>
-            {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li> */}
             <li>
               <a className="smoothscroll" href="#testimonials">
                 Testimonials
@@ -61,10 +60,6 @@ class Header extends Component {
         <div className="row banner">
           <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-//             <h3>
-              {/* {/* I'm a {city} based  */}
-//               <span>{occupation}</span>. {description}.
-//             </h3>
             <hr />
             <ul className="social">{networks}</ul>
           </div>
